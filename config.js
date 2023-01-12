7e9ff7c33c3c4d900aa9bdf0f433e29946900da4
阿卡林刘 <ScottLiu_NonWin@outlook.com>
2023-01-12 09:56:43 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '阿卡林刘 AkarinLiu.com',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1971493-a700aea86d78f9805fc39d13',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/AkarinLiu'
+    },
+    {
+      text: 'Blog',
+      url: 'https://www.akarinliu.com/'
+    },
+  ],
+};
